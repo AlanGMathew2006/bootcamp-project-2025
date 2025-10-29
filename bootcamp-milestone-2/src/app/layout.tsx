b49c8c5275620a055f1a35991d6ce57555b9476e
Alan Mathew <alangmathew@gmail.com>
2025-10-29 00:59:18 -0700

    commit -m"Updated folder and file structure"

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"; // Object to set metadata
 import { Inter } from "next/font/google"; // You can change the font to anything you want.
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/layout/navbar";
 
 // Use Inter font from Google Fonts
 const inter = Inter({ subsets: ["latin"] });
