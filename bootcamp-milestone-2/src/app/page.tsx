603d13d38e8b178f0a67fa6822c01992445514c2
Alan Mathew <alangmathew@gmail.com>
2025-10-29 01:44:08 -0700

    commit -m"Adjusted home page layout and styles"

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,8 +1,30 @@
-// You can name the function within page.tsx anything you want.
+import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/ui/Button";
+
 export default function Home() {
   return (
-		<div>
-			// content will go within here.
-		</div>
-		) 
+    <div className={styles["about-content"]}>
+      <div className={styles.left}>
+        <h3>Hello, I'm</h3>
+        <h1>Alan Mathew</h1>
+        <h3>Web Developer & Designer</h3>
+        <p>
+          Welcome to my personal website! I'm passionate about creating
+          beautiful and functional web experiences. Involved in Hack4Impact, a
+          non-profit organization focused on providing tech solutions for social
+          good. Feel free to explore my work!
+        </p>
+        <Button href="/portfolio">View My Portfolio</Button>
+      </div>
+      <div className={styles.right}>
+        <img
+          src="/images/home-page.png"
+          alt="Alan Mathew"
+          width="350"
+          height="525"
+        />
+      </div>
+    </div>
+  );
 }
